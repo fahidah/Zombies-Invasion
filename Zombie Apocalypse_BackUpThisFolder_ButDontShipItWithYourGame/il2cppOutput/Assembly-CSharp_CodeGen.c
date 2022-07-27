3048f3dd43804a40519adeca0b1b658aabfbbeb3
Faheedat Abdulsalam O <fahidah.abdussalam@gmail.com>
2022-07-27 16:12:26 +0100

    Switching to vuforia sdk

---
+++ Zombie Apocalypse_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,145 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 UnityEngine.GameObject AnchorCreator::get_AnchorPrefab()
+extern void AnchorCreator_get_AnchorPrefab_mBBAF7805E9D9F8D79408EE642D153BC76306B25A (void);
+// 0x00000002 System.Void AnchorCreator::set_AnchorPrefab(UnityEngine.GameObject)
+extern void AnchorCreator_set_AnchorPrefab_m53CC9CC3022C713826B44536B48B462C8A1FACFF (void);
+// 0x00000003 System.Void AnchorCreator::RemoveAllAnchors()
+extern void AnchorCreator_RemoveAllAnchors_m6BA302DD9EECA47969FD3F6E5397441DCB09ED21 (void);
+// 0x00000004 System.Void AnchorCreator::Awake()
+extern void AnchorCreator_Awake_m8820A3F157354D7EDED7B2D7CE2BFA42844F1F8E (void);
+// 0x00000005 System.Void AnchorCreator::Update()
+extern void AnchorCreator_Update_mA0A8BCCBAB0AE50DB087524E6273D11F1D6456D0 (void);
+// 0x00000006 System.Void AnchorCreator::.ctor()
+extern void AnchorCreator__ctor_m702B01425680D7FD1C9272A423AAF8A913E5476E (void);
+// 0x00000007 System.Void AnchorCreator::.cctor()
+extern void AnchorCreator__cctor_mF5AE519F086E52A01EA091115062D84D9F094F34 (void);
+// 0x00000008 System.Single ARFeatheredPlaneMeshVisualizer::get_featheringWidth()
+extern void ARFeatheredPlaneMeshVisualizer_get_featheringWidth_m14D3A8BE3E9A745E6FD525B19ADDC892B8399B4D (void);
+// 0x00000009 System.Void ARFeatheredPlaneMeshVisualizer::set_featheringWidth(System.Single)
+extern void ARFeatheredPlaneMeshVisualizer_set_featheringWidth_mD616A09A3B426EA5DE1FA37334DD194E43EEC110 (void);
+// 0x0000000A System.Void ARFeatheredPlaneMeshVisualizer::Awake()
+extern void ARFeatheredPlaneMeshVisualizer_Awake_mC5DB0414A2514BF4851266C25141C903F0AC57BA (void);
+// 0x0000000B System.Void ARFeatheredPlaneMeshVisualizer::OnEnable()
+extern void ARFeatheredPlaneMeshVisualizer_OnEnable_m8781C85CFED871C8A81A5B88DB1031856E0FC9F0 (void);
+// 0x0000000C System.Void ARFeatheredPlaneMeshVisualizer::OnDisable()
+extern void ARFeatheredPlaneMeshVisualizer_OnDisable_m2343B05B1A8F14BAD4DD516C584281B66FE6A4E8 (void);
+// 0x0000000D System.Void ARFeatheredPlaneMeshVisualizer::ARPlane_boundaryUpdated(UnityEngine.XR.ARFoundation.ARPlaneBoundaryChangedEventArgs)
+extern void ARFeatheredPlaneMeshVisualizer_ARPlane_boundaryUpdated_mB3D9BBD14EA1FE3ECDBACC2DB89C1B110B8B6B5F (void);
+// 0x0000000E System.Void ARFeatheredPlaneMeshVisualizer::GenerateBoundaryUVs(UnityEngine.Mesh)
+extern void ARFeatheredPlaneMeshVisualizer_GenerateBoundaryUVs_mF756D3C1F7925A69CD8C7C8CCE56209AB321FEF5 (void);
+// 0x0000000F System.Void ARFeatheredPlaneMeshVisualizer::.ctor()
+extern void ARFeatheredPlaneMeshVisualizer__ctor_m9A77651BCAE58AA0B994FFF6C6B63B1CFF2729F6 (void);
+// 0x00000010 System.Void ARFeatheredPlaneMeshVisualizer::.cctor()
+extern void ARFeatheredPlaneMeshVisualizer__cctor_mF4BA6DDB611A3FD966C8B9494AE6B3EB8647CEAD (void);
+// 0x00000011 System.Void AnimationScript::Start()
+extern void AnimationScript_Start_mAC1F54F92A07A5FB5D1138452446EA95F958B1B0 (void);
+// 0x00000012 System.Void AnimationScript::Update()
+extern void AnimationScript_Update_mB9A18E1CE8DD2F09B394721AC0B5188755EA7C9A (void);
+// 0x00000013 System.Void AnimationScript::.ctor()
+extern void AnimationScript__ctor_m3ADFA1175E3125D6C25902D78F6171CE65F841B3 (void);
+// 0x00000014 UnityEngine.GameObject PlacementController::get_PlacedPrefab()
+extern void PlacementController_get_PlacedPrefab_m61F17F4E8C5326BFD66A766A9DBFEFF989DE2C7C (void);
+// 0x00000015 System.Void PlacementController::set_PlacedPrefab(UnityEngine.GameObject)
+extern void PlacementController_set_PlacedPrefab_mB56FDD726BF41FE5770090C06C8C45A6FE35FE54 (void);
+// 0x00000016 System.Void PlacementController::Awake()
+extern void PlacementController_Awake_m30FDECDB403E2B12568E5D8214828729CE30D452 (void);
+// 0x00000017 System.Boolean PlacementController::TryGetTouchPosition(UnityEngine.Vector2&)
+extern void PlacementController_TryGetTouchPosition_m4E30D3BD44EDCBFFCDAD7697444D3FEFAC0DC8AD (void);
+// 0x00000018 System.Void PlacementController::Update()
+extern void PlacementController_Update_m2A9A8C06C56556E710F96B592E5FBAA17898D775 (void);
+// 0x00000019 System.Void PlacementController::.ctor()
+extern void PlacementController__ctor_m958492B38043C6E15A525ECC2CBF773BA764E823 (void);
+// 0x0000001A System.Void PlacementController::.cctor()
+extern void PlacementController__cctor_mDC75602062FE2277A01CB51C9303D99119107898 (void);
+static Il2CppMethodPointer s_methodPointers[26] = 
+{
+	AnchorCreator_get_AnchorPrefab_mBBAF7805E9D9F8D79408EE642D153BC76306B25A,
+	AnchorCreator_set_AnchorPrefab_m53CC9CC3022C713826B44536B48B462C8A1FACFF,
+	AnchorCreator_RemoveAllAnchors_m6BA302DD9EECA47969FD3F6E5397441DCB09ED21,
+	AnchorCreator_Awake_m8820A3F157354D7EDED7B2D7CE2BFA42844F1F8E,
+	AnchorCreator_Update_mA0A8BCCBAB0AE50DB087524E6273D11F1D6456D0,
+	AnchorCreator__ctor_m702B01425680D7FD1C9272A423AAF8A913E5476E,
+	AnchorCreator__cctor_mF5AE519F086E52A01EA091115062D84D9F094F34,
+	ARFeatheredPlaneMeshVisualizer_get_featheringWidth_m14D3A8BE3E9A745E6FD525B19ADDC892B8399B4D,
+	ARFeatheredPlaneMeshVisualizer_set_featheringWidth_mD616A09A3B426EA5DE1FA37334DD194E43EEC110,
+	ARFeatheredPlaneMeshVisualizer_Awake_mC5DB0414A2514BF4851266C25141C903F0AC57BA,
+	ARFeatheredPlaneMeshVisualizer_OnEnable_m8781C85CFED871C8A81A5B88DB1031856E0FC9F0,
+	ARFeatheredPlaneMeshVisualizer_OnDisable_m2343B05B1A8F14BAD4DD516C584281B66FE6A4E8,
+	ARFeatheredPlaneMeshVisualizer_ARPlane_boundaryUpdated_mB3D9BBD14EA1FE3ECDBACC2DB89C1B110B8B6B5F,
+	ARFeatheredPlaneMeshVisualizer_GenerateBoundaryUVs_mF756D3C1F7925A69CD8C7C8CCE56209AB321FEF5,
+	ARFeatheredPlaneMeshVisualizer__ctor_m9A77651BCAE58AA0B994FFF6C6B63B1CFF2729F6,
+	ARFeatheredPlaneMeshVisualizer__cctor_mF4BA6DDB611A3FD966C8B9494AE6B3EB8647CEAD,
+	AnimationScript_Start_mAC1F54F92A07A5FB5D1138452446EA95F958B1B0,
+	AnimationScript_Update_mB9A18E1CE8DD2F09B394721AC0B5188755EA7C9A,
+	AnimationScript__ctor_m3ADFA1175E3125D6C25902D78F6171CE65F841B3,
+	PlacementController_get_PlacedPrefab_m61F17F4E8C5326BFD66A766A9DBFEFF989DE2C7C,
+	PlacementController_set_PlacedPrefab_mB56FDD726BF41FE5770090C06C8C45A6FE35FE54,
+	PlacementController_Awake_m30FDECDB403E2B12568E5D8214828729CE30D452,
+	PlacementController_TryGetTouchPosition_m4E30D3BD44EDCBFFCDAD7697444D3FEFAC0DC8AD,
+	PlacementController_Update_m2A9A8C06C56556E710F96B592E5FBAA17898D775,
+	PlacementController__ctor_m958492B38043C6E15A525ECC2CBF773BA764E823,
+	PlacementController__cctor_mDC75602062FE2277A01CB51C9303D99119107898,
+};
+static const int32_t s_InvokerIndices[26] = 
+{
+	5629,
+	4530,
+	5729,
+	5729,
+	5729,
+	5729,
+	8977,
+	5673,
+	4566,
+	5729,
+	5729,
+	5729,
+	4413,
+	4530,
+	5729,
+	8977,
+	5729,
+	5729,
+	5729,
+	5629,
+	4530,
+	5729,
+	3022,
+	5729,
+	5729,
+	8977,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	26,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
